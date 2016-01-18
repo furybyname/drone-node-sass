835606776899d54ec4eaef07f029100951de42d5
Dave Archer <davearcher79@gmail.com>
2016-01-18 15:58:07 +0000

    splitting up

--- run.js
+++ run.js
@@ -8,11 +8,12 @@ var vargs = payload.vargs;
 var exec = require('child_process').exec;
 var srcPath = payload.workspace.path;
 var target = vargs.target || '';
-console.log('running npm install && grunt ' + target + ' && npm prune');
 
 console.log('running npm install');
+var optionsWith500kBuffer = {maxBuffer: 1024 * 500};
+
 var cmd_npmInstall = 'cd ' + srcPath + ' && npm install';
-exec(cmd_npmInstall, function(npmError, npmStdout, npmStderr) {
+exec(cmd_npmInstall, optionsWith500kBuffer, function(npmError, npmStdout, npmStderr) {
     if (npmError) {
         console.log(npmError);
         process.exit(1);
@@ -22,7 +23,7 @@ exec(cmd_npmInstall, function(npmError, npmStdout, npmStderr) {
 
     console.log('running grunt');
     var cmd_grunt = 'cd ' + srcPath + ' && npm install && node_modules/.bin/grunt ' + target;
-    exec(cmd_grunt, function(gruntError, gruntStdout, gruntStderr) {
+    exec(cmd_grunt, optionsWith500kBuffer, function(gruntError, gruntStdout, gruntStderr) {
         if (gruntError) {
             console.log(gruntError);
             process.exit(1);
@@ -32,7 +33,7 @@ exec(cmd_npmInstall, function(npmError, npmStdout, npmStderr) {
 
         console.log('running npm prune');
         var cmd_prune = 'cd ' + srcPath + ' && npm prune --production';
-        exec(cmd_prune, function(pruneError, pruneStdout, pruneStderr) {
+        exec(cmd_prune, optionsWith500kBuffer, function(pruneError, pruneStdout, pruneStderr) {
             if (pruneError) {
                 console.log(pruneError);
                 process.exit(1);
